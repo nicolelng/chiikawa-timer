226abcc84ad675c2684e29844b2f9b906571f6b6
Nicole <47255597+nicolelng@users.noreply.github.com>
2025-05-27 16:33:25 -0700

    Rename sounds and delete unneeded filles

--- sounds/sounds.js
+++ sounds/sounds.js
@@ -2,16 +2,16 @@ export const sounds = [
   {
     id: "sound1",
     name: "Arrival",
-    file: "sounds/simple-notification-152054.mp3",
+    file: "sounds/arrival.mp3",
   },
   {
     id: "sound2",
     name: "Ring",
-    file: "sounds/chime.mp3",
+    file: "sounds/ring.mp3",
   },
   {
     id: "sound3",
     name: "Pop",
-    file: "sounds/level-up-191997.mp3",
+    file: "sounds/pop.mp3",
   },
 ];
